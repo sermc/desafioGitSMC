ebd3081bd04044f04c7547598fac2fc4dc26677b
smoracar <smoracar@emeal.nttdata.com>
2022-03-29 14:37:47 +0200

    content

--- src/desafioGitSMC/DesafioGitSMC.java
+++ src/desafioGitSMC/DesafioGitSMC.java
@@ -4,7 +4,7 @@ public class DesafioGitSMC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo SMC");
+		System.out.println("Hola Mundo Sergio Mora Carrero");
 	}
 
 }
